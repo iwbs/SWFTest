848ca106c96655eb292803b231e3ab07abd555ef
Joseph Yuen <joseph.yuen@redflaggroup.com>
2016-02-02 18:07:22 +0800

    initial commit

---
+++ .apt_generated/helloWorld/HelloWorld/GreeterWorkflowClientExternalFactory.java
@@ -0,0 +1,13 @@
+/**
+ * This code was generated from {@link helloWorld.HelloWorld.GreeterWorkflow}.
+ *
+ * Any changes made directly to this file will be lost when 
+ * the code is regenerated.
+ */
+package helloWorld.HelloWorld;
+
+import com.amazonaws.services.simpleworkflow.flow.WorkflowClientFactoryExternal;
+
+public interface GreeterWorkflowClientExternalFactory extends WorkflowClientFactoryExternal<GreeterWorkflowClientExternal> {
+
+}
---
+++ .apt_generated/helloWorld/HelloWorld/GreeterWorkflowClientFactory.java
@@ -0,0 +1,13 @@
+/**
+ * This code was generated from {@link helloWorld.HelloWorld.GreeterWorkflow}.
+ *
+ * Any changes made directly to this file will be lost when 
+ * the code is regenerated.
+ */
+package helloWorld.HelloWorld;
+
+import com.amazonaws.services.simpleworkflow.flow.WorkflowClientFactory;
+
+public interface GreeterWorkflowClientFactory extends WorkflowClientFactory<GreeterWorkflowClient> {
+    
+}
